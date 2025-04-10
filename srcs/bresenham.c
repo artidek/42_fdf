927c3b2fc39790c6fe98acdf784334e733e6f7f1
aobshatk <obshatko.artem @gmail.com>
2025-04-11 00:39:13 +0200

    optimized bresenham's algorithm for building in all sectros

--- srcs/bresenham.c
+++ srcs/bresenham.c
@@ -6,104 +6,41 @@
 /*   By: aobshatk <aobshatk@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 07:17:34 by aobshatk          #+#    #+#             */
-/*   Updated: 2025/04/09 09:28:02 by aobshatk         ###   ########.fr       */
+/*   Updated: 2025/04/10 23:13:32 by aobshatk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fdf.h"
 
-static int	direction(int d)
+void draw_line(t_pix xy, t_addr *addr)
 {
-	if (d < 0)
-		return (-1);
-	else
-		return (1);
-}
+	int dx = abs(xy.x1 - xy.x);
+	int dy = abs(xy.y1 - xy.y);
+	int sx = (xy.x1 > xy.x) ? 1 : -1;
+	int sy = (xy.y1 > xy.y) ? 1 : -1;
+	int err = dx - dy;
+	int err2;
 
-static t_pix	check_xy(t_pix xy)
-{
-	int	temp;
-
-	if (xy.x > xy.x1 || xy.y > xy.y1)
+	while (1)
 	{
-		temp = xy.x;
-		xy.x = xy.x1;
-		xy.x1 = temp;
-		temp = xy.y;
-		xy.y = xy.y1;
-		xy.y1 = temp;
-	}
-	return (xy);
-}
-
-static void	draw_line_h(t_pix xy, t_addr *addr, int dx, int dy)
-{
-	int	p;
-	int	i;
-	int	dir;
-	int	y;
-	int	pix_addr;
-
-	dir = direction(dy);
-	y = xy.y;
-	dy *= dir;
-	p = 2 * dy - dx;
-	i = 0;
-	while (i <= dx)
-	{
-		pix_addr = (y * addr->line_size) + ((xy.x + i) * (addr->bpp / 8));
+		int pix_addr = (xy.y * addr->line_size) + (xy.x * (addr->bpp / 8));
 		addr->data[pix_addr] = (xy.color) & 0xFF;
 		addr->data[pix_addr + 1] = (xy.color >> 8) & 0xFF;
 		addr->data[pix_addr + 2] = (xy.color >> 16) & 0xFF;
-		i++;
-		if (p >= 0)
+
+		if (xy.x == xy.x1 && xy.y == xy.y1)
+			break;
+
+		err2 = 2 * err;
+		if (err2 > -dy)
 		{
-			y += dir;
-			p -= 2 * dx;
+			err -= dy;
+			xy.x += sx;
 		}
-		p += 2 * dy;
-	}
-}
-
-static void	draw_line_v(t_pix xy, t_addr *addr, int dx, int dy)
-{
-	int	p;
-	int	i;
-	int	dir;
-	int	x;
-	int	pix_addr;
-
-	dir = direction(dx);
-	x = xy.x;
-	dx *= dir;
-	p = 2 * dx - dy;
-	i = 0;
-	while (i <= dy)
-	{
-		pix_addr = ((xy.y + i) * addr->line_size) + (x * (addr->bpp / 8));
-		addr->data[pix_addr] = (xy.color) & 0xFF;
-		addr->data[pix_addr + 1] = (xy.color >> 8) & 0xFF;
-		addr->data[pix_addr + 2] = (xy.color >> 16) & 0xFF;
-		i++;
-		if (p >= 0)
+		if (err2 < dx)
 		{
-			x += dir;
-			p -= 2 * dy;
+			err += dx;
+			xy.y += sy;
 		}
-		p += 2 * dx;
-	}
-}
-
-void	draw_line(t_pix xy, t_addr *addr)
-{
-	if (abs(xy.x1 - xy.x) > abs(xy.y1 - xy.y))
-	{
-		xy = check_xy(xy);
-		draw_line_h(xy, addr, xy.x1 - xy.x, xy.y1 - xy.y);
-	}
-	else
-	{
-		xy = check_xy(xy);
-		draw_line_v(xy, addr, xy.x1 - xy.x, xy.y1 - xy.y);
 	}
 }
